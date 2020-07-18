e23255f6e9cad24f8996f5f562623989e5b05852
kacper pirog <kacper.pirog@gmail.com>
2020-07-18 16:25:58 +0000

    add class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 'use strict';
 
 
-  const titleClickHandler = function(event){
+const titleClickHandler = function(event){
     console.log('Link was clicked!');
-    /* remove class 'active' from all article links */ 
+/* remove class 'active' from all article links */ 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
     /* add class 'active' to the clicked link */
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}
     /* get 'href' attribute from the clicked links */
     /* find the correct article using the selector (vakue of 'href' attribute) */
     /* add class 'active' to the correct article */
