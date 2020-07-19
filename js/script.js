d6267e3156ddc004005bc0f555cb0925b182b613
kacper pirog <kacper.pirog@gmail.com>
2020-07-19 19:14:57 +0000

    add class actove

--- js/script.js
+++ js/script.js
@@ -2,28 +2,39 @@
 
 
 const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
-/* remove class 'active' from all article links */ 
+    
+/* [done] remove class 'active' from all article links */ 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
-    /* add class 'active' to the clicked link */
-    /* remove class 'active' from all articles */
+    /* [DONE]add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+    
+    /* [done]remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
-
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
-    /* get 'href' attribute from the clicked links */
+    /* [DONE] get 'href' attribute from the clicked links */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+
     /* find the correct article using the selector (vakue of 'href' attribute) */
+    const correctArticle = document.querySelector(articleSelector);
+    console.log('Correct article:', correctArticle);
+    
     /* add class 'active' to the correct article */
-    console.log(event);
+    correctArticle.classList.add('active');
   }
   
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
+for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
+}
